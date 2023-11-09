1c9c4cb8a2b108696fdc8342a19a30ea29e55a92
xuanLoc <loclx2911@gmail.com>
2023-11-09 19:10:25 +0700

    add file

---
+++ src/app/modules/product-portfolio/product-portfolio.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-portfolio',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-portfolio.component.html',
+  styleUrls: ['./product-portfolio.component.scss']
+})
+export class ProductPortfolioComponent {
+
+}
